6fffe7c6bea26f5ee544ef6dc19b883c4b9e16c1
LucasSKuhnke <lucas.kuhnke2005@gmail.com>
2025-01-24 12:08:39 +0100

    exeption examples

--- input_example.py
+++ input_example.py
@@ -13,3 +13,9 @@ except ValueError:
     print("I can also print this in case of error that I prevented")
 except ZeroDivisionError:
     print("you can not divide by 0!")
+except:
+    print("this is another type of error")
+else: # this is for no exception
+    print("Thank you for playing as expected")
+finally: # this will be executed no matter what, at the very end
+    print("Thanks for playing the game")
